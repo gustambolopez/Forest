868a35564b1bbb5c6f6ee414d636b9d44633fbde
gustambolopez <mogolixzq@gmail.com>
2025-05-18 18:36:39 -0500

    d

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -5,8 +5,8 @@ self.__uv$config = {
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
-  bundle: "/uv/uv.bundle.js",
   client: "/uv/uv.client.js",
+  bundle: "/uv/uv.bundle.js",
   config: "/uv/uv.config.js",
   sw: "/uv/uv.sw.js",
 };
